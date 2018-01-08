4bb4c5c0930f9255dc1f8d31993b5fc9735f3e36
David Aguilar <davidaguilarh.94@gmail.com>
2018-01-08 02:21:12 -0500

    Fix counter

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.update = this.update.bind(this)
+    this.state = {
+      value: '',
+      length: 0
+    }
+  }
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.update}>{this.state.value}</textarea>
+        <div className="counter">{this.state.length}</div>
       </div>
     );
+  }
+
+  update(event) {
+    this.setState({
+      value: event.target.value,
+      length: event.target.value.length
+    })
   }
 }
 
